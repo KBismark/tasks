331b3ea6519a4a37acc097d1d50f4db016769f94
KBismark <bismark.yamoah@amalitech.com>
2024-11-10 03:33:38 +0000

    Add more gallery items

--- labs/week1/image_gallery/assets/js/global.js
+++ labs/week1/image_gallery/assets/js/global.js
@@ -8,22 +8,45 @@ export const imageData = [
     {
         fullSize: 'assets/images/hero1-comp.jpg',
         thumbnail: 'assets/images/hero1-comp.jpg',
-        caption: 'Image 2 - City skyline at night',
+        caption: 'Image 2 - City campus colledge students',
         date: new Date('2024-03-16')
     },
     {
         fullSize: 'assets/images/hero1-comp.jpg',
         thumbnail: 'assets/images/hero1-comp.jpg',
-        caption: 'Image 3 - Mountain peaks',
+        caption: 'Image 3 - Mountain hills here now',
         date: new Date('2024-03-17')
     },
     {
         fullSize: 'assets/images/hero1-comp.jpg',
         thumbnail: 'assets/images/hero1-comp.jpg',
-        caption: 'Image 4 - Ocean waves',
+        caption: 'Image 4 - Oh Great holy Mary',
         date: new Date('2024-03-18')
     },
-    
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 5 - Dear great old friend, I miss you!',
+        date: new Date('2023-03-15')
+    },
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 6 - Awesome, Shinny, Flowers',
+        date: new Date('202e-03-26')
+    },
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 7 - Night nights not bad!',
+        date: new Date('2024-07-11')
+    },
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 8 - Veep of the United States of America',
+        date: new Date('2023-05-08')
+    },
 ];
 
 // Export DOM Elements and global variables
