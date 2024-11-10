c9a87a9df5878bbf18948175cd5c42d6d5f1fe08
KBismark <bismark.yamoah@amalitech.com>
2024-11-10 03:10:02 +0000

    Export global variables

---
+++ labs/week1/image_gallery/assets/js/global.js
@@ -0,0 +1,36 @@
+export const imageData = [
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 1 - A beautiful landscape',
+        date: new Date('2024-03-15')
+    },
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 2 - City skyline at night',
+        date: new Date('2024-03-16')
+    },
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 3 - Mountain peaks',
+        date: new Date('2024-03-17')
+    },
+    {
+        fullSize: 'assets/images/hero1-comp.jpg',
+        thumbnail: 'assets/images/hero1-comp.jpg',
+        caption: 'Image 4 - Ocean waves',
+        date: new Date('2024-03-18')
+    },
+    
+];
+
+// Export DOM Elements and global variables
+export const galleryGrid = document.getElementById('galleryGrid');
+export const lightbox = document.getElementById('lightbox');
+export const imageContainer = document.getElementById('imageContainer');
+export const lightboxCaption = document.getElementById('lightboxCaption');
+export const prevButton = document.getElementById('prevButton');
+export const nextButton = document.getElementById('nextButton');
+export const closeButton = document.getElementById('closeButton');
