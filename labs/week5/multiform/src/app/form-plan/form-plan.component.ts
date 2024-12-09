db456ec1e75725e93c8d5077a733d6ba0af0f9cb
KBismark <bismark.yamoah@amalitech.com>
2024-12-09 17:00:01 +0000

    Add plan options ui

--- labs/week5/multiform/src/app/form-plan/form-plan.component.ts
+++ labs/week5/multiform/src/app/form-plan/form-plan.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-form-plan',
+  selector: 'form-plan.content',
   standalone: true,
   imports: [],
   templateUrl: './form-plan.component.html',
   styleUrl: './form-plan.component.css'
 })
 export class FormPlanComponent {
+  yearlyPlan = false
+  //Methods
+  onToggle(){
 
+  }
 }
--- labs/week5/multiform/src/app/form/form.component.ts
+++ labs/week5/multiform/src/app/form/form.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormInfoComponent } from "../form-info/form-info.component";
 import { AppstoreService } from '../services/appstore.service';
+import { FormPlanComponent } from "../form-plan/form-plan.component";
 
 @Component({
   selector: 'formcontent',
   standalone: true,
-  imports: [CommonModule, FormInfoComponent],
+  imports: [CommonModule, FormInfoComponent, FormPlanComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
