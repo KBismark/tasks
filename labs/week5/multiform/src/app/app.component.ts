206de24633b6f88a16f473a72a944148b06eb690
KBismark <bismark.yamoah@amalitech.com>
2024-12-09 08:10:33 +0000

    Add source files

--- labs/week5/multiform/src/app/app.component.ts
+++ labs/week5/multiform/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavPanelComponent } from "./nav-panel/nav-panel.component";
+import { FormComponent } from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavPanelComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
