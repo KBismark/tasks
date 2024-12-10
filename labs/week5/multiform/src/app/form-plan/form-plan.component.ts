c8e3ecd0880aa3cdc68665909f6caf45a08080b1
KBismark <bismark.yamoah@amalitech.com>
2024-12-10 10:30:06 +0000

    Update subscription plan page to match ui

--- labs/week5/multiform/src/app/form-plan/form-plan.component.ts
+++ labs/week5/multiform/src/app/form-plan/form-plan.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AppstoreService } from '../services/appstore.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'form-plan.content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form-plan.component.html',
   styleUrl: './form-plan.component.css'
 })
 export class FormPlanComponent {
-  yearlyPlan = false
+  appStoreService = inject(AppstoreService)
+
   //Methods
   onToggle(){
-
+    const {planType} = this.appStoreService
+    this.appStoreService.planType = planType === 'monthly'?'yearly':'monthly'
   }
 }
