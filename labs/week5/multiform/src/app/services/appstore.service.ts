a28d6a080ff8a9c121c7c1e77a8ef134147df924
KBismark <bismark.yamoah@amalitech.com>
2024-12-10 10:26:17 +0000

    Add subscription plan properties

--- labs/week5/multiform/src/app/services/appstore.service.ts
+++ labs/week5/multiform/src/app/services/appstore.service.ts
@@ -8,5 +8,8 @@ const SELECTIONS = {INFO: 1, PLAN: 2, ADDONS: 3, SUMMARY: 4}
 export class AppstoreService {
   SELECTIONS = SELECTIONS
   selected = SELECTIONS.INFO
+  planCategory: 'arcade'|'advanced'|'pro' = 'arcade'
+  planType: 'yearly'|'monthly' = 'monthly'
+  detailsConfirmed = false
   constructor() { }
 }
