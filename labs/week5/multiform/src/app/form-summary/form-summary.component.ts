a04bda741b2eaa9ccd8271d35083a771d0d4071b
KBismark <bismark.yamoah@amalitech.com>
2024-12-10 15:35:01 +0000

    Display price of selected plan and add-ons

--- labs/week5/multiform/src/app/form-summary/form-summary.component.ts
+++ labs/week5/multiform/src/app/form-summary/form-summary.component.ts
@@ -12,4 +12,15 @@ import { AppstoreService } from '../services/appstore.service';
 export class FormSummaryComponent {
   appStoreService = inject(AppstoreService)
 
+  get selectedAddons(){
+    return this.appStoreService.addons.filter(item=>item.selected)
+  }
+
+  get totalPrice(){
+    return this.selectedAddons.reduce((pre,current)=>pre+current.price, 0) + this.selectedPlanPrice
+  }
+
+  get selectedPlanPrice(){
+    return this.appStoreService.prices[this.appStoreService.planType][this.appStoreService.planCategory]
+  }
 }
