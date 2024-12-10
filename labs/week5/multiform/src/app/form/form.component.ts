02ec2dfd3a4cbc7bc5d484712466fc03ddeac281
KBismark <bismark.yamoah@amalitech.com>
2024-12-10 10:27:40 +0000

    Add all required screens

--- labs/week5/multiform/src/app/form/form.component.ts
+++ labs/week5/multiform/src/app/form/form.component.ts
@@ -3,11 +3,13 @@ import { Component, inject } from '@angular/core';
 import { FormInfoComponent } from "../form-info/form-info.component";
 import { AppstoreService } from '../services/appstore.service';
 import { FormPlanComponent } from "../form-plan/form-plan.component";
+import { FormAddonsComponent } from "../form-addons/form-addons.component";
+import { FormSummaryComponent } from "../form-summary/form-summary.component";
 
 @Component({
   selector: 'formcontent',
   standalone: true,
-  imports: [CommonModule, FormInfoComponent, FormPlanComponent],
+  imports: [CommonModule, FormInfoComponent, FormPlanComponent, FormAddonsComponent, FormSummaryComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
