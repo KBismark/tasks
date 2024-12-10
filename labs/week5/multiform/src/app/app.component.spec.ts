ec834948b0289d677765d8651f5645c93a2cc68b
KBismark <bismark.yamoah@amalitech.com>
2024-12-10 12:36:36 +0000

    Use reactive forms and validate form inputs

--- labs/week5/multiform/src/app/app.component.spec.ts
+++ labs/week5/multiform/src/app/app.component.spec.ts
@@ -13,13 +13,6 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'multiform' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('multiform');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- labs/week5/multiform/src/app/services/appstore.service.ts
+++ labs/week5/multiform/src/app/services/appstore.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, Signal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 const SELECTIONS = {INFO: 1, PLAN: 2, ADDONS: 3, SUMMARY: 4}
 
@@ -11,5 +12,36 @@ export class AppstoreService {
   planCategory: 'arcade'|'advanced'|'pro' = 'arcade'
   planType: 'yearly'|'monthly' = 'monthly'
   detailsConfirmed = false
-  constructor() { }
+
+  userPersonalInfo = signal(new FormGroup({
+    name : new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(64),
+    ]),
+    email : new FormControl('',[
+      Validators.required,
+      Validators.email,
+      Validators.minLength(3),
+      Validators.maxLength(64)
+    ]),
+    phone : new FormControl('',[
+      Validators.required,
+      Validators.min(10),
+      Validators.maxLength(15),
+    ])
+  }))
 }
+
+
+type FormValue = {
+  name: string | null;
+  email: string | null;
+  phone: string | null;
+}
+
+type PersonalInfo = FormGroup<{
+  name: FormControl<string | null>;
+  email: FormControl<string | null>;
+  phone: FormControl<string | null>;
+}>
