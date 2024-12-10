6fea3789973785ce9e2c1e817dcac35cbb641dc6
KBismark <bismark.yamoah@amalitech.com>
2024-12-10 10:28:13 +0000

    Add summary page

--- labs/week5/multiform/src/app/form-summary/form-summary.component.ts
+++ labs/week5/multiform/src/app/form-summary/form-summary.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AppstoreService } from '../services/appstore.service';
 
 @Component({
-  selector: 'app-form-summary',
+  selector: 'form-summary.content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form-summary.component.html',
   styleUrl: './form-summary.component.css'
 })
 export class FormSummaryComponent {
+  appStoreService = inject(AppstoreService)
 
 }
